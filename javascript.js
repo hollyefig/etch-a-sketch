7bf8b036794c3fcd220980a52fb6186392abd0cc
“Holly <hollyefig@gmail.com>
2022-11-12 18:10:41 -0500

    updated html to have color picker, JS to have color picker options. HTML is commented out

--- javascript.js
+++ javascript.js
@@ -5,7 +5,8 @@ let input = document.querySelector("#input"),
   colorPicker = document.getElementById("colorPicker"),
   cellColor = colorPicker.value,
   eraserButton = document.getElementById("eraser"),
-  clearButton = document.getElementById("save");
+  clearButton = document.getElementById("save"),
+  colorsWrapper = document.getElementById("colorsWrapper");
 
 const draw = (e) => (e.target.style.backgroundColor = cellColor);
 const createGrid = () => {
@@ -14,7 +15,7 @@ const createGrid = () => {
   gridWrapper.style.cssText = `grid-template-rows: repeat(${gridSize}, auto);
     grid-template-columns: repeat(${gridSize}, auto);`;
 
-  // create box and array to hold boxes
+  // create boxes
   let cells = gridSize * gridSize;
   for (i = 0; i < cells; i++) {
     const square = document.createElement("div");
@@ -28,8 +29,6 @@ const changeGridSize = (n) => {
   createGrid();
 };
 
-const changeColor = () => (cellColor = colorPicker.value);
-
 const erase = () => {
   cellColor === colorPicker.value
     ? (cellColor = "#f6f6f6")
@@ -41,5 +40,42 @@ const clearGrid = () => {
   while (gridWrapper.firstChild)
     gridWrapper.removeChild(gridWrapper.firstChild);
 };
+
+// color saver and picker
+const colorSet = (e) => {
+  function ColorToHex(color) {
+    let hexadecimal = color.toString(16);
+    return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+  }
+
+  function ConvertRGBtoHex(red, green, blue) {
+    return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
+  }
+
+  if (e.style.backgroundColor === "") {
+    e.style.backgroundColor = colorPicker.value;
+  } else {
+    console.log("color taken");
+    let rgb = e.style.backgroundColor;
+    let r = rgb.slice(4, 7);
+    let g = rgb.slice(9, 12);
+    let b = rgb.slice(14, 17);
+    let newValue = ConvertRGBtoHex(Number(r), Number(g), Number(b));
+    colorPicker.value = newValue;
+  }
+};
+
+console.log("original value", colorPicker.value);
+// create radios for color slots
+for (let i = 1; i <= 10; i++) {
+  const colorSlot = document.createElement("input");
+  colorSlot.setAttribute("type", "radio");
+  colorSlot.setAttribute("id", `color${i}`);
+
+  colorSlot.setAttribute("onclick", "colorSet(this)");
+  colorsWrapper.appendChild(colorSlot);
+}
+
+const changeColor = () => (cellColor = colorPicker.value);
 
 createGrid();
